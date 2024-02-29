d509a0deaa80ae4aa146e7ae43019b734912d747
Integralsen <161722715+Integralsen@users.noreply.github.com>
2024-02-29 21:46:31 +0900

    Create osero.php

---
+++ osero.php
@@ -0,0 +1,22 @@
+<?php
+class Osero{
+    public function getValue($x, $y){
+        $result[$x][$y] = "";
+
+        if($x == 3 && $y == 3){
+            $result[$x][$y] = "○";
+        }
+        if($x == 3 && $y == 4){
+            $result[$x][$y] = "●";
+        }
+        if($x == 4 && $y == 3){
+            $result[$x][$y] = "●";
+        }
+        if($x == 4 && $y == 4){
+            $result[$x][$y] = "○";
+        }
+
+        return $result[$x][$y];
+    }
+}
+?>
